07622d27d0e7727571c492cbf3b9e83ab27086d0
vasiuddin7733 <vasiuddin7733>
2023-05-09 12:54:48 +0200

    create form with email and password

--- frontend/src/components/page2/comp21.jsx
+++ frontend/src/components/page2/comp21.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const  Comp21 = () => {
+const Comp21 = () => {
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+  const changeHandler = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(data);
+  };
   return (
-    <div>Comp21</div>
-  )
-}
+    <div>
+      <form onSubmit={onSubmit}>
+        <label>Email</label>
+        <br />
+        <input
+          type="email"
+          placeholder="email"
+          name="email"
+          onClick={changeHandler}
+        />
+        <br />
+        <label>Password</label>
+        <br />
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          onClick={changeHandler}
+        />
+        <br />
+        <input type="submit" />
+      </form>
+    </div>
+  );
+};
 
-export default Comp21
+export default Comp21;
